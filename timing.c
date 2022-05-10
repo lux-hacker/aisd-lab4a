ced3a87dbebe2e9b1966ee93b2f769bb49179998
Pustovit Vladislav <pvs013@campus.mephi.ru>
2022-05-10 17:47:12 +0300

    timing

--- timing.c
+++ timing.c
@@ -3,101 +3,100 @@
 #include <time.h>
 #include "controller/controller.h"
 
-BinTree* generateTree(int);
+BinTree *generateTree(int);
 void timingAddElement();
-/*void timingDeleteElement();
-void timingFindElement();*/
+void timingDeleteElement();
+void timingFindElement();
 
-int main(){
-   srand(time(NULL));
-   printf("======[TIMING]======\n");
-   printf("0)Exit\n");
-   printf("1)Add element\n");
-   printf("2)Delete element\n");
-   printf("3)Find element\n");
-   printf("--> ");
-   int n = getUInt();
-   switch(n){
-	case 1:{
-	    timingAddElement();
-	    break;
-	}
-	/*case 2:{
+int main() {
+    srand(time(NULL));
+    printf("======[TIMING]======\n");
+    printf("0)Exit\n");
+    printf("1)Add element\n");
+    printf("2)Delete element\n");
+    printf("3)Find element\n");
+    printf("--> ");
+    int n = getUInt();
+    switch (n) {
+        case 1: {
+            timingAddElement();
+            break;
+        }
+        case 2: {
             timingDeleteElement();
             break;
         }
-	case 3:{
+        case 3: {
             timingFindElement();
             break;
-        }*/
-	default:{
-	    break;
-	}
-   }
-   return 0;
+        }
+        default: {
+            break;
+        }
+    }
+    return 0;
 }
 
-BinTree* generateTree(int size){
-    FILE* buf = fopen("buf.bin", "w+b");
-    BinTree* tree = (BinTree*) malloc(sizeof(BinTree));
+BinTree *generateTree(int size) {
+    FILE *buf = fopen("buf.bin", "w+b");
+    BinTree *tree = (BinTree *) malloc(sizeof(BinTree));
     tree->root = NULL;
-    for(int i = 0; i < size; i++){
-	unsigned int key = rand();
-	unsigned int data = key;
-	fwrite(&key, sizeof(unsigned int), 1, buf);
-	addNewElement(tree, key, data);
+    for (int i = 0; i < size; i++) {
+        unsigned int key = rand();
+        unsigned int data = key;
+        fwrite(&key, sizeof(unsigned int), 1, buf);
+        addNewElement(tree, key, data);
     }
     fclose(buf);
     return tree;
 }
 
-void timingAddElement(){
-    FILE* fd = fopen("timing.csv", "w+");
+void timingAddElement() {
+    FILE *fd = fopen("timing.csv", "w+");
     fprintf(fd, "len,avg,func\n");
-    for(int size = 100000; size <= 5000000; size+=100000){
-	double sum = 0;
-	for(int k = 0; k < 10; k++){
-	    BinTree* tree = generateTree(size);
-	    unsigned int keys[100] = {0};
-	    for(int i = 0; i < 100; i++){
-		keys[i] = rand();
-	    }
-	    clock_t start = clock();
-	    for(int i = 0; i < 100; i++){
-		addNewElement(tree, keys[i], keys[i]);
-	    }
-	    clock_t end = clock();
-	    sum += (double)(end - start) / CLOCKS_PER_SEC;
-	    freeTree(tree);
-	    free(tree);
-	}
-	fprintf(fd, "%d,%f,%s\n", size, sum/10, "addElement");
+    for (int size = 100000; size <= 5000000; size += 100000) {
+        double sum = 0;
+        for (int k = 0; k < 10; k++) {
+            BinTree *tree = generateTree(size);
+            unsigned int keys[100] = {0};
+            for (int i = 0; i < 100; i++) {
+                keys[i] = rand();
+            }
+            clock_t start = clock();
+            for (int i = 0; i < 100; i++) {
+                addNewElement(tree, keys[i], keys[i]);
+            }
+            clock_t end = clock();
+            sum += (double) (end - start) / CLOCKS_PER_SEC;
+            freeTree(tree);
+            free(tree);
+        }
+        fprintf(fd, "%d,%f,%s\n", size, sum / 10, "addElement");
     }
     fclose(fd);
 }
-/*
+
 void timingDeleteElement(){
     FILE* fd = fopen("timing.csv", "w+");
     fprintf(fd, "len,avg,func\n");
-    for(int i = 1; i <= 10; i++){
-        int size = 100000*i;
+    for (int size = 100000; size <= 5000000; size += 100000) {
         double sum = 0;
-        for(int j = 0; j < 10; j++){
-            BinTree* tree = generateTree(size);
-            int offset = rand() % size;
-	    FILE* buf = fopen("buf.bin", "r");
-	    fseek(buf, offset * sizeof(unsigned int), SEEK_SET);
-	    unsigned int key;
-	    fread(&key, sizeof(unsigned int), 1, buf);
-	    fclose(buf);
-            clock_t begin = clock();
-            deleteElement(tree, key);
+        for (int k = 0; k < 10; k++) {
+            BinTree *tree = generateTree(size);
+            unsigned int keys[100] = {0};
+            for (int i = 0; i < 100; i++) {
+                keys[i] = rand();
+            }
+            clock_t start = clock();
+            for (int i = 0; i < 100; i++) {
+                deleteElement(tree, keys[i]);
+            }
             clock_t end = clock();
-            sum += (double)(end-begin) / CLOCKS_PER_SEC;
+            sum += (double) (end - start) / CLOCKS_PER_SEC;
             freeTree(tree);
             free(tree);
-	}
-        fprintf(fd, "%d,%f,%s\n", size, sum/10, "deleteElement");
+        }
+        fprintf(fd, "%d,%f,%s\n", size, sum / 10, "addElement");
     }
     fclose(fd);
 }
@@ -105,26 +104,24 @@ void timingDeleteElement(){
 void timingFindElement(){
     FILE* fd = fopen("timing.csv", "w+");
     fprintf(fd, "len,avg,func\n");
-    for(int i = 1; i <= 10; i++){
-        int size = 100000*i;
+    for (int size = 100000; size <= 5000000; size += 100000) {
         double sum = 0;
-        for(int j = 0; j < 10; j++){
-            BinTree* tree = generateTree(size);
-            int offset = rand() % size;
-            FILE* buf = fopen("buf.bin", "r");
-            fseek(buf, offset * sizeof(unsigned int), SEEK_SET);
-            unsigned int key;
-            fread(&key, sizeof(unsigned int), 1, buf);
-            fclose(buf);
-            clock_t begin = clock();
-            findByKey(tree, key, 1);
+        for (int k = 0; k < 10; k++) {
+            BinTree *tree = generateTree(size);
+            unsigned int keys[100] = {0};
+            for (int i = 0; i < 100; i++) {
+                keys[i] = rand();
+            }
+            clock_t start = clock();
+            for (int i = 0; i < 100; i++) {
+                findByKey(tree, keys[i], 1);
+            }
             clock_t end = clock();
-            sum += (double)(end-begin) / CLOCKS_PER_SEC;
+            sum += (double) (end - start) / CLOCKS_PER_SEC;
             freeTree(tree);
             free(tree);
         }
-        fprintf(fd, "%d,%f,%s\n", size, sum/10, "findElement");
+        fprintf(fd, "%d,%f,%s\n", size, sum / 10, "addElement");
     }
     fclose(fd);
 }
-*/
